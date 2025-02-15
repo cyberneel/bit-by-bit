8606ed92d275d843433dfca1e2435aa49b9a9081
Elitelord <sameeragarwal007@outlook.com>
2025-02-15 17:49:32 -0600

    forgot push

--- scripts/playground_camera.gd
+++ scripts/playground_camera.gd
@@ -125,4 +125,5 @@ func _unhandled_input(event: InputEvent) -> void:
 
 
 func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 	pass # Replace with function body.
