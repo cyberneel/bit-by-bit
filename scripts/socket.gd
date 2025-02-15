349ea2a57bc898079a450ec1bb98e6430c9b72bf
Neelesh Chevuri <neelesh.chevuri25@gmail.com>
2025-02-15 17:51:11 -0600

    Finished base and node

--- scripts/socket.gd
+++ scripts/socket.gd
@@ -2,15 +2,21 @@ extends Node
 
 class_name Socket
 
-var status: bool
-var _in: Socket
+@export var status: bool
+@export var _in: Socket
 
 func _init():
 	status = false
 	_in = null
 
 func check():
-	if(_in == null):
-		status = false
-	else:
+	if(_in):
 		status = _in.status
+		
+	if status:
+		$Sprite2D.modulate = Color.SILVER
+	else:
+		$Sprite2D.modulate = Color.BLACK
+		
+func _process(delta: float) -> void:
+	check()
